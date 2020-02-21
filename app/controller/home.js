244f0cbb549f6ef8ee7b09607090473812d8f939
wu.chaoliang <chaoliangwu9088@gmail.com>
2020-02-21 23:04:09 +0800

    链接本地数据库

---
+++ app/controller/home.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const Controller = require('egg').Controller;
+
+class HomeController extends Controller {
+  async index() {
+    const { ctx } = this;
+    console.log(3333, this.app.mysql.get('article'));
+    const res = await this.app.mysql.get('article', { id: 1 });
+    await this.app.mysql.insert('article', { title: 'Hello World',title: 'Hello World',introduce: 'introduce11'});
+    // const res = await this.app.mysql.insert('react', { title: 'Hello World' });
+    console.log('res', res);
+    ctx.body = 'hi, egg';
+  }
+}
+
+module.exports = HomeController;
---
+++ config/config.default.js
@@ -0,0 +1,48 @@
+/* eslint valid-jsdoc: "off" */
+
+'use strict';
+
+/**
+ * @param {Egg.EggAppInfo} appInfo app info
+ */
+module.exports = appInfo => {
+  /**
+   * built-in config
+   * @type {Egg.EggAppConfig}
+   **/
+  const config = exports = {};
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1579965155194_7789';
+
+  // add your middleware config here
+  config.middleware = [];
+
+  // add your user config here
+  const userConfig = {
+    // myAppName: 'egg',
+  };
+  config.mysql = {
+    // 单数据库信息配置
+    client: {
+      // host
+      host: 'localhost',
+      // 端口号
+      port: '3308',
+      // 用户名
+      user: 'root',
+      // 密码
+      password: '123456',
+      // 数据库名
+      database: 'blogData',
+    },
+    // 是否加载到 app 上，默认开启
+    app: true,
+    // 是否加载到 agent 上，默认关闭
+    agent: false,
+  };
+  return {
+    ...config,
+    ...userConfig,
+  };
+};
