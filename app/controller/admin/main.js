5c2cc0b8d50b95296f171ed03cb2a54c6ce821f0
wu.chaoliang <chaoliangwu9088@gmail.com>
2020-02-23 15:02:30 +0800

    后台登录接口

---
+++ app/controller/admin/main.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const Controller = require('egg').Controller;
+
+class MainController extends Controller {
+  async index() {
+    this.ctx.body = 'hi app';
+  }
+  async checkLogin() {
+    const userName = this.ctx.request.body.userName || 'test';
+    const password = this.ctx.request.body.password || 'aB123456';
+    const sql = "SELECT userName FROM admin_user WHERE userName = '" + userName + "' AND password = '" + password + "'";
+    const res = await this.app.mysql.query(sql);
+    console.log('res', res, userName, password);
+    this.ctx.body = 'hi checkLogin';
+    if (res.length > 0) {
+      const openId = new Date().getTime();
+      this.ctx.session.openId = { openId };
+      this.ctx.body = { data: '登录成功', openId };
+    } else {
+      this.ctx.body = { data: '登录失败' };
+    }
+  }
+}
+
+module.exports = MainController;
+
