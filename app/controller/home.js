1ffc217aedfcd3fb4dbcb385c4addd51f650283f
wu.chaoliang <chaoliangwu9088@gmail.com>
2020-03-07 17:05:03 +0800

    up

--- app/controller/home.js
+++ app/controller/home.js
@@ -5,8 +5,9 @@ const Controller = require('egg').Controller;
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
+    console.log('ctx', ctx.params);
     // console.log(3333, this.app.mysql.get('block'));
-    const res = await this.app.mysql.get('article', { id: 3 });
+    const res = await this.app.mysql.get('article', { id: ctx.params.id });
     // await this.app.mysql.insert('article', { id: 3, title: 'Hello World', introduce: 'introduce11', content: 'contentcontent' });
     // const res = await this.app.mysql.insert('react', { title: 'Hello World' });
     // console.log('res', res);
