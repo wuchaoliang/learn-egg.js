43ad0e1cc62c6a57e4e3c8f15d9c49e4b4bda1d8
wu.chaoliang <chaoliangwu9088@gmail.com>
2020-05-03 19:00:47 +0800

    sequelize db:seed生成测试数据

--- app/migrations/20200503101920-create-user.js
+++ app/migrations/20200503101920-create-user.js
@@ -6,19 +6,34 @@ module.exports = {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       username: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.CHAR(32),
+        allowNull: false,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
+    }, {
+      tableName: 'users',
+      charset: 'utf8mb4',
+      collate: 'utf8mb4_bin',
+    }).then(() => {
+      return queryInterface.addIndex('users', {
+        name: 'username',
+        unique: true, // 该索引为唯一，不允许有相同的username
+        fields: [ 'username' ],
+      })
     });
   },
   down: (queryInterface, Sequelize) => {
---
+++ app/seeders/20200503105009-User.js
@@ -0,0 +1,35 @@
+'use strict';
+const md5 = require('md5');
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert('Users', [{
+      username: 'John',
+      password: md5('123456'),
+    },
+    {
+      username: 'Jack',
+      password: md5('654321'),
+    }], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+  }
+};
